43573d897476a03a9d2bc42e6ec6ea877394681d
sunnymac <sanjaymakwana.it@gmail.com>
2021-11-02 12:47:33 +0530

    1st commit

---
+++ routes/routes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../screens/splash_screen.dart';
+import '../screens/register_screen.dart';
+import '../screens/login_screen.dart';
+
+class RouteManager {
+  static const String splashScreen = "/";
+  static const String registerScreen = "/registerScreen";
+  static const String loginScreen = "/loginScreen";
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case splashScreen:
+        return MaterialPageRoute(
+          builder: (context) => const SplashScreen(),
+        );
+
+      case registerScreen:
+        return MaterialPageRoute(
+          builder: (context) => const RegisterScreen(),
+        );
+
+      case loginScreen:
+        return MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        );
+
+      default:
+        throw const FormatException("Invalid Route!");
+    }
+  }
+}
